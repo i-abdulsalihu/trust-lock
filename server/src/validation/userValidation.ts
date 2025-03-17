7e4e5cbd1236d72ca3b0063dd540a583bd7c4be6
cakezero <sareahj.com@gmail.com>
2025-03-17 11:54:45 +0100

    user validation schema checker

---
+++ server/src/validation/userValidation.ts
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+const userValidation = Joi.object({
+  firstName: Joi.string().trim().required(),
+  lastName: Joi.string().trim().required(),
+  bio: Joi.string().trim().required(),
+  occupation: Joi.string().trim().required(),
+  email: Joi.string().email().trim().required(),
+  username: Joi.string().trim().required(),
+});
+
+export default userValidation;
