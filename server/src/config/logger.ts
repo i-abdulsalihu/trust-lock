3ebef5e2b0731dd2785b001706167cdfbfdd6281
cakezero <sareahj.com@gmail.com>
2025-03-17 03:10:06 +0100

    db and logger config

---
+++ server/src/config/logger.ts
@@ -0,0 +1,28 @@
+import winston from "winston";
+import { ENVIRONMENT } from "../utils/env";
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: ENVIRONMENT === "development" ? "debug" : "info",
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    ENVIRONMENT === "development"
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ["error"],
+    }),
+  ],
+});
+
+export default logger;
