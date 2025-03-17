bcb7d4707911aeeaa267feaf89292d0f3dc1b8ae
cakezero <sareahj.com@gmail.com>
2025-03-17 03:09:38 +0100

    server full setup with cors for security

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import { PORT }  from "./src/utils/env";
+import logger from "./src/config/logger";
+import DB from "./src/config/db";
+import apiRoutes from "./src/routes/api";
+
+const server = express();
+
+server.use(cors({ origin: ["http://localhost:3000", "https://trust-lock.vercel.app"] }));
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use("/api", apiRoutes);
+
+server.listen(PORT, async () => {
+  logger.info(`✅ Server is running on port ${PORT}`);
+  await DB();
+})
